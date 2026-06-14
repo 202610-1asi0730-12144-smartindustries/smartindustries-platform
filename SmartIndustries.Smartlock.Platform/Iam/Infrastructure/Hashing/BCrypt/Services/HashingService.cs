c378b7859ad8f3ca562f8584a7c39d147889250f
ImaLi06 <imanollimache@gmail.com>
2026-06-14 12:53:35 -0500

    feat(iam): create HashingService

---
+++ SmartIndustries.Smartlock.Platform/Iam/Infrastructure/Hashing/BCrypt/Services/HashingService.cs
@@ -0,0 +1,11 @@
+using SmartIndustries.Smartlock.Platform.Iam.Application.Internal.OutboundServices;
+using BCryptNet = BCrypt.Net.BCrypt;
+
+namespace SmartIndustries.Smartlock.Platform.Iam.Infrastructure.Hashing.BCrypt.Services;
+
+public class HashingService : IHashingService
+{
+    public string HashPassword(string password) => BCryptNet.HashPassword(password);
+
+    public bool VerifyPassword(string password, string passwordHash) => BCryptNet.Verify(password, passwordHash);
+}
