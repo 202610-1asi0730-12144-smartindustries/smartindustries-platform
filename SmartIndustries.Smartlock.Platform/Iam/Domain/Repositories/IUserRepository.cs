8178cc7e9c8cbff3b0b4dccbdf0c1014cac9ee52
ImaLi06 <imanollimache@gmail.com>
2026-06-14 12:29:34 -0500

    feat(iam): create IUserRepository

---
+++ SmartIndustries.Smartlock.Platform/Iam/Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,9 @@
+using SmartIndustries.Smartlock.Platform.Iam.Domain.Model.Aggregates;
+using SmartIndustries.Smartlock.Platform.Shared.Domain.Repositories;
+
+namespace SmartIndustries.Smartlock.Platform.Iam.Domain.Repositories;
+
+public interface IUserRepository : IBaseRepository<User>
+{
+    
+}
