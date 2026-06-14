e5284e4c76e8c71c4748b6606d2539da4fb534d4
ImaLi06 <imanollimache@gmail.com>
2026-06-14 12:49:41 -0500

    feat(iam): create IHashingService

---
+++ SmartIndustries.Smartlock.Platform/Iam/Application/Internal/OutboundServices/IHashingService.cs
@@ -0,0 +1,8 @@
+namespace SmartIndustries.Smartlock.Platform.Iam.Application.Internal.OutboundServices;
+
+public interface IHashingService
+{
+    string HashPassword(string password);
+    
+    bool VerifyPassword(string password, string passwordHash);
+}
