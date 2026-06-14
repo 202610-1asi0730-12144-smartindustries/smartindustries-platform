a1c30be2979a972ff2a024d30281be6f7c0c6651
ImaLi06 <imanollimache@gmail.com>
2026-06-14 14:48:06 -0500

    feat(iam): create AllowAnonymousAttribute

---
+++ SmartIndustries.Smartlock.Platform/Iam/Infrastructure/Pipeline/Middleware/Attributes/AllowAnonymousAttribute.cs
@@ -0,0 +1,6 @@
+namespace SmartIndustries.Smartlock.Platform.Iam.Infrastructure.Pipeline.Middleware.Attributes;
+
+[AttributeUsage(AttributeTargets.Method)]
+public class AllowAnonymousAttribute : Attribute
+{
+}
