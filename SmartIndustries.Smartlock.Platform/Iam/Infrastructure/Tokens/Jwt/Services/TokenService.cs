5285008870c32f85b548d1844ee41b288d3d175f
ImaLi06 <imanollimache@gmail.com>
2026-06-14 12:47:46 -0500

    feat(iam): create TokenService

---
+++ SmartIndustries.Smartlock.Platform/Iam/Infrastructure/Tokens/Jwt/Services/TokenService.cs
@@ -0,0 +1,60 @@
+using System.Security.Claims;
+using System.Text;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+using SmartIndustries.Smartlock.Platform.Iam.Application.Internal.OutboundServices;
+using SmartIndustries.Smartlock.Platform.Iam.Domain.Model.Aggregates;
+using SmartIndustries.Smartlock.Platform.Iam.Infrastructure.Tokens.Jwt.Configuration;
+
+namespace SmartIndustries.Smartlock.Platform.Iam.Infrastructure.Tokens.Jwt.Services;
+
+public class TokenService(IOptions<TokenSettings> tokenSettings) : ITokenService
+{
+    private readonly TokenSettings _tokenSettings = tokenSettings.Value;
+
+    public string GenerateToken(User user)
+    {
+        var secret = _tokenSettings.Secret;
+        var key = Encoding.ASCII.GetBytes(secret);
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new[]
+            {
+                new Claim(ClaimTypes.Sid, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.Email.Value)
+            }),
+            Expires = DateTime.UtcNow.AddDays(7),
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+        };
+        var tokenHandler = new JsonWebTokenHandler();
+        return tokenHandler.CreateToken(tokenDescriptor);
+    }
+
+    public async Task<long?> ValidateToken(string token)
+    {
+        if (string.IsNullOrEmpty(token)) return null;
+
+        var tokenHandler = new JsonWebTokenHandler();
+        var key = Encoding.ASCII.GetBytes(_tokenSettings.Secret);
+        try
+        {
+            var tokenValidationResult = await tokenHandler.ValidateTokenAsync(token, new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ClockSkew = TimeSpan.Zero
+            });
+
+            var jwtToken = (JsonWebToken)tokenValidationResult.SecurityToken;
+            var userId = long.Parse(jwtToken.Claims.First(claim => claim.Type == ClaimTypes.Sid).Value);
+            return userId;
+        }
+        catch (Exception)
+        {
+            return null;
+        }
+    }
+}
