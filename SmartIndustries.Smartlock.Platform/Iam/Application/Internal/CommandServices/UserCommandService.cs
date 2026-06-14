7fbfa5b780e4096121f5eb092d83225b6c46193b
ImaLi06 <imanollimache@gmail.com>
2026-06-14 13:05:56 -0500

    feat(iam): create UserCommandService with SignUpCommand

---
+++ SmartIndustries.Smartlock.Platform/Iam/Application/Internal/CommandServices/UserCommandService.cs
@@ -0,0 +1,49 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Localization;
+using SmartIndustries.Smartlock.Platform.Iam.Application.CommandServices;
+using SmartIndustries.Smartlock.Platform.Iam.Application.Internal.OutboundServices;
+using SmartIndustries.Smartlock.Platform.Iam.Domain.Model;
+using SmartIndustries.Smartlock.Platform.Iam.Domain.Model.Aggregates;
+using SmartIndustries.Smartlock.Platform.Iam.Domain.Model.Commands;
+using SmartIndustries.Smartlock.Platform.Iam.Domain.Repositories;
+using SmartIndustries.Smartlock.Platform.Shared.Application.Model;
+using SmartIndustries.Smartlock.Platform.Shared.Domain.Repositories;
+using SmartIndustries.Smartlock.Platform.Shared.Resources.Errors;
+
+namespace SmartIndustries.Smartlock.Platform.Iam.Application.Internal.CommandServices;
+
+public class UserCommandService(
+    IUserRepository userRepository,
+    ITokenService tokenService,
+    IHashingService hashingService,
+    IUnitOfWork unitOfWork,
+    IStringLocalizer<ErrorMessages> localizer) : IUserCommandService
+{
+    public async Task<Result> Handle(SignUpCommand command, CancellationToken cancellationToken)
+    {
+        if (await userRepository.ExistsByEmailAsync(command.Email, cancellationToken))
+            return Result.Failure(IamError.EmailAlreadyTaken,
+                localizer["IamError.EmailAlreadyTaken", command.Email]);
+
+        var hashedPassword = hashingService.HashPassword(command.Password);
+        var user = new User(command.FirstName, command.LastName, hashedPassword, command.Email);
+        try
+        {
+            await userRepository.AddAsync(user, cancellationToken);
+            await unitOfWork.CompleteAsync(cancellationToken);
+            return Result.Success();
+        }
+        catch (OperationCanceledException)
+        {
+            return Result.Failure(IamError.OperationCancelled, localizer["IamError.OperationCancelled"]);
+        }
+        catch (DbUpdateException)
+        {
+            return Result.Failure(IamError.DatabaseError, localizer["IamError.DatabaseError"]);
+        }
+        catch (Exception)
+        {
+            return Result.Failure(IamError.InternalServerError, localizer["IamError.InternalServerError"]);
+        }
+    }
+}
--- SmartIndustries.Smartlock.Platform/Iam/Domain/Repositories/IUserRepository.cs
+++ SmartIndustries.Smartlock.Platform/Iam/Domain/Repositories/IUserRepository.cs
@@ -1,9 +1,11 @@
 using SmartIndustries.Smartlock.Platform.Iam.Domain.Model.Aggregates;
+using SmartIndustries.Smartlock.Platform.Shared.Domain.Model.ValueObjects;
 using SmartIndustries.Smartlock.Platform.Shared.Domain.Repositories;
 
 namespace SmartIndustries.Smartlock.Platform.Iam.Domain.Repositories;
 
 public interface IUserRepository : IBaseRepository<User>
 {
-    
+    Task<bool> ExistsByEmailAsync(string email, CancellationToken cancellationToken);
+    Task<User?> FindByEmailAsync(Email email, CancellationToken cancellationToken = default);
 }
