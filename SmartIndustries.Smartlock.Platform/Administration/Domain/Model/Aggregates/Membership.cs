a5f57ec4184322faa7f10c03859092c20f03f55f
ImaLi06 <imanollimache@gmail.com>
2026-06-15 12:42:43 -0500

    feat(administration): create Membership aggregate

---
+++ SmartIndustries.Smartlock.Platform/Administration/Domain/Model/Aggregates/Membership.cs
@@ -0,0 +1,16 @@
+namespace SmartIndustries.Smartlock.Platform.Administration.Domain.Model.Aggregates;
+
+public partial class Membership
+{
+    public long Id { get; private set; }
+    public long UserId { get; private set; }
+    public long RoleId { get; private set; }
+
+    public Membership(long userId, long roleId)
+    {
+        UserId = userId;
+        RoleId = roleId;
+    }
+
+    private Membership() { }
+}
