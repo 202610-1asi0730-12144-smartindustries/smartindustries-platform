7f7ff38538c7edaac2bc48c84351f3954ee88062
ImaLi06 <imanollimache@gmail.com>
2026-06-15 12:51:38 -0500

    feat(administration): create MembershipRepository

---
+++ SmartIndustries.Smartlock.Platform/Administration/Domain/Repositories/IMembershipRepository.cs
@@ -0,0 +1,8 @@
+using SmartIndustries.Smartlock.Platform.Administration.Domain.Model.Aggregates;
+using SmartIndustries.Smartlock.Platform.Shared.Domain.Repositories;
+
+namespace SmartIndustries.Smartlock.Platform.Administration.Domain.Repositories;
+
+public interface IMembershipRepository : IBaseRepository<Membership>
+{
+}
