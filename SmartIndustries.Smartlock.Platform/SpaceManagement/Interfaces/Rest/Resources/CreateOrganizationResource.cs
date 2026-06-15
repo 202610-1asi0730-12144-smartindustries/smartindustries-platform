e4564c3c6cfe1a933956db2ba6b37d2d397ace9d
ImaLi06 <imanollimache@gmail.com>
2026-06-15 13:11:46 -0500

    feat(administration): create OrganizationResource and CreateOrganiationResource

---
+++ SmartIndustries.Smartlock.Platform/SpaceManagement/Interfaces/Rest/Resources/CreateOrganizationResource.cs
@@ -0,0 +1,3 @@
+namespace SmartIndustries.Smartlock.Platform.SpaceManagement.Interfaces.Rest.Resources;
+
+public record CreateOrganizationResource(string Name, string Description);
---
+++ SmartIndustries.Smartlock.Platform/SpaceManagement/Interfaces/Rest/Resources/OrganizationResource.cs
@@ -0,0 +1,3 @@
+namespace SmartIndustries.Smartlock.Platform.SpaceManagement.Interfaces.Rest.Resources;
+
+public record OrganizationResource(long Id, string Name, string Description);
