cf10971af41a7792499040069d14890efda63fb3
ImaLi06 <imanollimache@gmail.com>
2026-06-15 12:09:54 -0500

    feat(administration): create Role aggregate

---
+++ SmartIndustries.Smartlock.Platform/Administration/Domain/Model/Aggregates/Role.cs
@@ -0,0 +1,24 @@
+using SmartIndustries.Smartlock.Platform.Administration.Domain.Model.ValueObjects;
+using SmartIndustries.Smartlock.Platform.Shared.Domain.Model.ValueObjects;
+
+namespace SmartIndustries.Smartlock.Platform.Administration.Domain.Model.Aggregates;
+
+public partial class Role
+{
+    public long Id { get; private set; }
+    public long OrganizationId { get; private set; }
+    public GenericName Name { get; private set; }
+    public RolePermissions Permissions { get; private set; }
+    public bool Deletable { get; private set; }
+
+    private Role() { }
+
+    public static Role CreateRoot(long organizationId)
+        => new()
+        {
+            OrganizationId = organizationId,
+            Name = new GenericName("Root"),
+            Permissions = new RolePermissions(true, true, true),
+            Deletable = false
+        };
+}
