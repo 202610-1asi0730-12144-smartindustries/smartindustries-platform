123d5c05398dd0542b957c7af0897f930241ec0e
ImaLi06 <imanollimache@gmail.com>
2026-06-15 11:33:26 -0500

    feat(spacemanagement): create IOrganizationRepository

---
+++ SmartIndustries.Smartlock.Platform/SpaceManagement/Domain/Repositories/IOrganizationRepository.cs
@@ -0,0 +1,9 @@
+using SmartIndustries.Smartlock.Platform.Shared.Domain.Repositories;
+using SmartIndustries.Smartlock.Platform.SpaceManagement.Domain.Model.Aggregates;
+
+namespace SmartIndustries.Smartlock.Platform.SpaceManagement.Domain.Repositories;
+
+public interface IOrganizationRepository : IBaseRepository<Organization>
+{
+    Task<bool> ExistsByNameAsync(string name, CancellationToken cancellationToken);
+}
