2fb42c827b8c2c7d5db1fc94c7195e6ce99b3527
ImaLi06 <imanollimache@gmail.com>
2026-06-20 10:36:30 -0500

    feat(shared): create Day value object

---
+++ SmartIndustries.Smartlock.Platform/Shared/Domain/Model/ValueObjects/Day.cs
@@ -0,0 +1,12 @@
+namespace SmartIndustries.Smartlock.Platform.Shared.Domain.Model.ValueObjects;
+
+public enum Day
+{
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+}
