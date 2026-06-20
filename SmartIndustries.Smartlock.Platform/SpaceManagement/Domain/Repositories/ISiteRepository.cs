10ee03b37798b5463208ba3ae72eab2eaab27f50
ImaLi06 <imanollimache@gmail.com>
2026-06-20 18:07:39 -0500

    feat(space-management): add FindByOrgnaizationIdAsync to SiteRepository

--- SmartIndustries.Smartlock.Platform/SpaceManagement/Domain/Repositories/ISiteRepository.cs
+++ SmartIndustries.Smartlock.Platform/SpaceManagement/Domain/Repositories/ISiteRepository.cs
@@ -5,4 +5,5 @@ namespace SmartIndustries.Smartlock.Platform.SpaceManagement.Domain.Repositories
 
 public interface ISiteRepository : IBaseRepository<Site>
 {
+    Task<IEnumerable<Site>> FindByOrganizationIdAsync(long organizationId, CancellationToken cancellationToken = default);
 }
--- SmartIndustries.Smartlock.Platform/SpaceManagement/Infrastructure/Persistence/EntityFrameworkCore/Repositories/SiteRepository.cs
+++ SmartIndustries.Smartlock.Platform/SpaceManagement/Infrastructure/Persistence/EntityFrameworkCore/Repositories/SiteRepository.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using SmartIndustries.Smartlock.Platform.Shared.Infrastructure.Persistence.EntityFramerWorkCore.Configuration;
 using SmartIndustries.Smartlock.Platform.Shared.Infrastructure.Persistence.EntityFramerWorkCore.Repositories;
 using SmartIndustries.Smartlock.Platform.SpaceManagement.Domain.Model.Aggregates;
@@ -7,4 +8,6 @@ namespace SmartIndustries.Smartlock.Platform.SpaceManagement.Infrastructure.Pers
 
 public class SiteRepository(AppDbContext context) : BaseRepository<Site>(context), ISiteRepository
 {
+    public async Task<IEnumerable<Site>> FindByOrganizationIdAsync(long organizationId, CancellationToken cancellationToken = default)
+        => await context.Set<Site>().Where(site => site.OrganizationId == organizationId).ToListAsync(cancellationToken);
 }
