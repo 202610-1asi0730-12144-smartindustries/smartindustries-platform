1655427da959a021c79b1272050e0d3f64b8da13
ImaLi06 <imanollimache@gmail.com>
2026-06-20 17:12:55 -0500

    feat(administration): add AddRoleToOrganization to RoleCommandService

--- SmartIndustries.Smartlock.Platform/Administration/Application/Internal/CommandServices/RoleCommandService.cs
+++ SmartIndustries.Smartlock.Platform/Administration/Application/Internal/CommandServices/RoleCommandService.cs
@@ -6,6 +6,7 @@ using SmartIndustries.Smartlock.Platform.Administration.Domain.Model;
 using SmartIndustries.Smartlock.Platform.Administration.Domain.Model.Aggregates;
 using SmartIndustries.Smartlock.Platform.Administration.Domain.Model.Commands;
 using SmartIndustries.Smartlock.Platform.Administration.Domain.Model.Events;
+using SmartIndustries.Smartlock.Platform.Administration.Domain.Model.ValueObjects;
 using SmartIndustries.Smartlock.Platform.Administration.Domain.Repositories;
 using SmartIndustries.Smartlock.Platform.Shared.Application.Model;
 using SmartIndustries.Smartlock.Platform.Shared.Domain.Repositories;
@@ -55,6 +56,35 @@ public class RoleCommandService(
         try
         {
             var role = Role.CreateBasic(command.OrganizationId);
+            await roleRepository.AddAsync(role, cancellationToken);
+            await unitOfWork.CompleteAsync(cancellationToken);
+
+            return Result<Role>.Success(role);
+        }
+        catch (ArgumentException exception)
+        {
+            return Result<Role>.Failure(AdministrationError.InvalidData, exception.Message);
+        }
+        catch (OperationCanceledException)
+        {
+            return Result<Role>.Failure(AdministrationError.OperationCancelled, localizer["AdministrationError.OperationCancelled"]);
+        }
+        catch (DbUpdateException)
+        {
+            return Result<Role>.Failure(AdministrationError.DatabaseError, localizer["AdministrationError.DatabaseError"]);
+        }
+        catch (Exception)
+        {
+            return Result<Role>.Failure(AdministrationError.InternalServerError, localizer["AdministrationError.InternalServerError"]);
+        }
+    }
+
+    public async Task<Result<Role>> Handle(AddRoleToOrganizationCommand command, CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            var permissions = new RolePermissions(command.CanCreateSites, command.CanCreatePeople, command.CanConnectDevices);
+            var role = Role.CreateCustom(command.OrganizationId, command.Name, permissions);
             await roleRepository.AddAsync(role, cancellationToken);
             await unitOfWork.CompleteAsync(cancellationToken);
 
