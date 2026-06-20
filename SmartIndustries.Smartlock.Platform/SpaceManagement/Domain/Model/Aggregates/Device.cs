fa27c79cb00ed59d4e23e47b2c8bea732612df2b
ImaLi06 <imanollimache@gmail.com>
2026-06-20 10:43:02 -0500

    feat(space-management): create Device aggregate

---
+++ SmartIndustries.Smartlock.Platform/SpaceManagement/Domain/Model/Aggregates/Device.cs
@@ -0,0 +1,23 @@
+using SmartIndustries.Smartlock.Platform.Shared.Domain.Model.ValueObjects;
+using SmartIndustries.Smartlock.Platform.SpaceManagement.Domain.Model.ValueObjects;
+
+namespace SmartIndustries.Smartlock.Platform.SpaceManagement.Domain.Model.Aggregates;
+
+public partial class Device
+{
+    public long Id { get; private set; }
+    public long SiteId { get; private set; }
+    public GenericName Name { get; private set; }
+    public DeviceStatus Status { get; private set; }
+    public DeviceMode Mode { get; private set; }
+
+    public Device(long siteId, string name, string mode)
+    {
+        SiteId = siteId;
+        Name = new GenericName(name);
+        Mode = Enum.Parse<DeviceMode>(mode, ignoreCase: true);
+        Status = DeviceStatus.Online;
+    }
+
+    private Device() { }
+}
