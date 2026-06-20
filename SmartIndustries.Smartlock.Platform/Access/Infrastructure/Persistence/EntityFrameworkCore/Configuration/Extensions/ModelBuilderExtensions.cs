bdba1bc26fb66e9463ad4958794fd6f9a4ed04a8
ImaLi06 <imanollimache@gmail.com>
2026-06-20 10:58:02 -0500

    feat(access): update ModelBuilderExtensions

--- SmartIndustries.Smartlock.Platform/Access/Infrastructure/Persistence/EntityFrameworkCore/Configuration/Extensions/ModelBuilderExtensions.cs
+++ SmartIndustries.Smartlock.Platform/Access/Infrastructure/Persistence/EntityFrameworkCore/Configuration/Extensions/ModelBuilderExtensions.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using SmartIndustries.Smartlock.Platform.Access.Domain.Model.Aggregates;
 
 namespace SmartIndustries.Smartlock.Platform.Access.Infrastructure.Persistence.EntityFrameworkCore.Configuration.Extensions;
 
@@ -6,6 +7,27 @@ public static class ModelBuilderExtensions
 {
     public static void ApplyAccessConfiguration(this ModelBuilder builder)
     {
-        
+        builder.Entity<AccessGroup>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Id).IsRequired().ValueGeneratedOnAdd();
+            entity.Property(e => e.OrganizationId).IsRequired();
+            entity.Property(e => e.Description).HasMaxLength(500);
+
+            entity.OwnsOne(e => e.Name, name =>
+            {
+                name.WithOwner().HasForeignKey("Id");
+                name.Property(n => n.Value).HasColumnName("name").IsRequired().HasMaxLength(100);
+            });
+        });
+
+        builder.Entity<PersonAccess>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Id).IsRequired().ValueGeneratedOnAdd();
+            entity.Property(e => e.PersonId).IsRequired();
+            entity.HasIndex(e => e.PersonId).IsUnique();
+            entity.Property(e => e.Status).HasConversion<string>().IsRequired().HasMaxLength(50);
+        });
     }
 }
