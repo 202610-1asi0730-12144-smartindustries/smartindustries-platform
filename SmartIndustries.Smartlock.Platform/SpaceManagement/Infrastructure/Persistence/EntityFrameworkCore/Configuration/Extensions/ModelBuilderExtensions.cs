ae04cd5293d6d9efeb6ee41a6ce2118343485a2b
ImaLi06 <imanollimache@gmail.com>
2026-06-20 10:59:23 -0500

    feat(space-management): update ModelBuilderExtensions

--- SmartIndustries.Smartlock.Platform/SpaceManagement/Infrastructure/Persistence/EntityFrameworkCore/Configuration/Extensions/ModelBuilderExtensions.cs
+++ SmartIndustries.Smartlock.Platform/SpaceManagement/Infrastructure/Persistence/EntityFrameworkCore/Configuration/Extensions/ModelBuilderExtensions.cs
@@ -18,5 +18,54 @@ public static class ModelBuilderExtensions
 
             entity.Property(e => e.Description).HasMaxLength(500);
         });
+
+        builder.Entity<Site>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Id).IsRequired().ValueGeneratedOnAdd();
+            entity.Property(e => e.OrganizationId).IsRequired();
+            entity.Property(e => e.Description).HasMaxLength(500);
+
+            entity.OwnsOne(e => e.Name, name =>
+            {
+                name.WithOwner().HasForeignKey("Id");
+                name.Property(n => n.Value).HasColumnName("name").IsRequired().HasMaxLength(100);
+            });
+        });
+
+        builder.Entity<Device>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Id).IsRequired().ValueGeneratedOnAdd();
+            entity.Property(e => e.SiteId).IsRequired();
+            entity.Property(e => e.Status).HasConversion<string>().IsRequired().HasMaxLength(50);
+            entity.Property(e => e.Mode).HasConversion<string>().IsRequired().HasMaxLength(50);
+
+            entity.OwnsOne(e => e.Name, name =>
+            {
+                name.WithOwner().HasForeignKey("Id");
+                name.Property(n => n.Value).HasColumnName("name").IsRequired().HasMaxLength(100);
+            });
+        });
+
+        builder.Entity<Person>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Id).IsRequired().ValueGeneratedOnAdd();
+            entity.Property(e => e.OrganizationId).IsRequired();
+
+            entity.OwnsOne(e => e.Name, name =>
+            {
+                name.WithOwner().HasForeignKey("Id");
+                name.Property(n => n.FirstName).HasColumnName("first_name").IsRequired().HasMaxLength(100);
+                name.Property(n => n.LastName).HasColumnName("last_name").IsRequired().HasMaxLength(100);
+            });
+
+            entity.OwnsOne(e => e.IdentityDocument, document =>
+            {
+                document.WithOwner().HasForeignKey("Id");
+                document.Property(d => d.Value).HasColumnName("identity_document").IsRequired().HasMaxLength(50);
+            });
+        });
     }
 }
